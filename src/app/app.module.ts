2eb3ce6b35e2055974c137b703652e3d9f6d5ad9
Gennady <algashev@live.com>
2018-09-29 15:59:41 +0400

    Переключение вкладок

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HeaderComponent } from './header/header.component';
 
 import { AppComponent } from './app.component';
-import {StartPageComponent} from './start-page/start-page.component';
-import {FooterComponent} from './footer/footer.component';
-import {PlacesComponent} from './places/places.component';
-import {DevelopmentsComponent} from './developments/developments.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {AuthModalContent} from './header/auth/auth.component';
-import {RegModalContent} from './header/reg/reg.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlacesComponent } from './places/places.component';
+import { DevelopmentsComponent } from './developments/developments.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthModalContent } from './header/auth/auth.component';
+import { RegModalContent } from './header/reg/reg.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '',
+    component: StartPageComponent
+  },
+  { path: 'start-page', component: StartPageComponent },
+  { path: 'developments', component: DevelopmentsComponent },
+  { path: 'places', component: PlacesComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -26,14 +37,18 @@ import {RegModalContent} from './header/reg/reg.component';
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     StartPageComponent,
     PlacesComponent,
-    DevelopmentsComponent
+    DevelopmentsComponent,
+    RouterModule
   ],
   providers: [],
   entryComponents: [AuthModalContent,RegModalContent],
