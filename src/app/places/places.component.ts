46b57fc99ca95086cd88c0b568c1898819a6393c
Gennady <algashev@live.com>
2018-09-29 17:05:00 +0400

    Добавление селектов и шапки в странице "Места"

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 
 export class PlacesComponent implements OnInit {
 
+  optionsSelect;
+
   ngOnInit(): void {
+    this.optionsSelect = [
+      { value: '1', label: 'Option 1' },
+      { value: '2', label: 'Option 2' },
+      { value: '3', label: 'Option 3' },
+    ];
   }
 
 }
